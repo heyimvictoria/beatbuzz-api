eb1392e067f3ea0370f06d7da82df368eb5d17d5
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2024-03-11 19:22:12 -0500

    spring added

---
+++ beat-buzz/src/main/java/org/launchcode/beatbuzz/BeatBuzzApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.beatbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatBuzzApplication.class, args);
+	}
+
+}
---
+++ beat-buzz/src/test/java/org/launchcode/beatbuzz/BeatBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.beatbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
