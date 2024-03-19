d790fc42dbdcbb442ec3abc9d68c94d9b2d1710f
EzekielCz <ezechielcazacu@gmail.com>
2024-03-18 20:18:52 -0500

    dependises

---
+++ src/main/java/models/User.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+
+    public User(){}
+
+    public User(String username, String pwHash) {
+        this.username = username;
+        this.pwHash = pwHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
