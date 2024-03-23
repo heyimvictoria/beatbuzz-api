0879fc15fa22d889ec1208ad49c9ba4f2d1fa13c
Aether1a <olkyrah@outlook.com>
2024-03-22 21:32:05 -0500

    Website can be accesed

---
+++ queries.sql
@@ -0,0 +1,19 @@
+-- Stored information on Users
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Stored information on album reviews
+CREATE TABLE reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    album_name VARCHAR(255) NOT NULL,
+    review_text TEXT,
+    rating INT,
+    review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,11 +1,19 @@
 package org.launchcode.models;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
+import java.io.Serializable;
+
 @Entity
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements Serializable {
     @NotNull
+    @Column(name = "username")
     private String username;
 
     @NotNull
