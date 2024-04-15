f738df0507cf9cfb0f353c7582b5076f4f1a1ec7
Aether1a <olkyrah@outlook.com>
2024-04-15 16:06:53 -0500

    finalized SQL Implementation

---
+++ src/main/java/org/launchcode/data/UserRepository.java
@@ -0,0 +1,9 @@
+package org.launchcode.data;
+
+import org.launchcode.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
