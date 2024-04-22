27f8a109522e08538c993664a1a4ddeaf97107ca
EzekielCz <ezechielcazacu@gmail.com>
2024-04-21 21:46:38 -0500

    final changes for username and password

--- src/main/java/org/launchcode/beatbuzzApplication.java
+++ src/main/java/org/launchcode/beatbuzzApplication.java
@@ -1,11 +1,9 @@
 package org.launchcode;
 
-import org.launchcode.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
+
 @SpringBootApplication
 public class beatbuzzApplication {
 
